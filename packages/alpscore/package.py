3d85d7877c087d4bf31812ca93fbda534d8b9f1f
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-07-25 12:03:23 -0400

    initial commit

---
+++ packages/alpscore/package.py
@@ -0,0 +1,17 @@
+from spack import *
+
+
+class alpscore(CMakePackage):
+    homepage = "https://github.com/ALPSCore/ALPSCore"
+    url      = "https://github.com/ALPSCore/ALPSCore/archive/v2.2.0.tar.gz"
+    git      = "https://github.com/ALPSCore/ALPSCore.git"
+
+    version('master',       branch='master')
+    version('2.2.0',        sha256='f7bc9c8f806fb0ad4d38cb6604a10d56ab159ca63aed6530c1f84ecaf40adc61')
+
+    depends_on('boost')
+    depends_on('eigen ~fftw ~metis ~mpfr ~scotch ~suitesparse')
+
+    def cmake_args(self):
+        args = []
+        return args
---
+++ packages/hpx/package.py
@@ -0,0 +1,20 @@
+from spack import *
+
+
+class HPX(CMakePackage):
+    """C++ runtime system for parallel and distributed applications."""
+
+    homepage = "http://stellar.cct.lsu.edu/tag/hpx/"
+    url      = "https://github.com/STEllAR-GROUP/hpx/archive/1.3.0.tar.gz"
+
+    version('1.3.0',     sha256='cd34da674064c4cc4a331402edbd65c5a1f8058fb46003314ca18fa08423c5ad')
+    version('1.2.0',     sha256='20942314bd90064d9775f63b0e58a8ea146af5260a4c84d0854f9f968077c170')
+
+    depends_on('boost')
+    depends_on('jemalloc')
+    depends_on('hwloc')
+    depends_on('mpi')
+
+    def cmake_args(self):
+        args = ['-DHPX_BUILD_EXAMPLES=OFF', '-DHPX_WITH_MALLOC=jemalloc', '-DHPX_WITH_MAX_CPU_COUNT=128', '-DHPX_WITH_EXAMPLES=OFF']
+        return args
