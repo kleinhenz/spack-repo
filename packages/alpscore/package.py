cafc52b494770d4c84b83b3db7b7ffd02d70398b
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-07-26 15:28:22 +0000

    [alpscore] add variants for ALPS_MODULES_DISABLE

--- packages/alpscore/package.py
+++ packages/alpscore/package.py
@@ -11,7 +11,18 @@ class Alpscore(CMakePackage):
 
     depends_on('boost')
     depends_on('eigen ~fftw ~metis ~mpfr ~scotch ~suitesparse')
+    variant('accumulators', default=False, description="Enable legacy accumulators")
+    variant('mc', default=False, description="Enable monte carlo")
 
     def cmake_args(self):
-        args = []
+        alps_modules_disable=["accumulators", "mc"]
+        if "+accumulators" in self.spec:
+            alps_modules_disable.remove("accumulators")
+        if "+mc" in self.spec:
+            alps_modules_disable.remove("mc")
+
+        args = ["-DALPS_CXX_STD=custom"]
+        if len(alps_modules_disable):
+            args.append("-DALPS_MODULES_DISABLE={:s}".format(";".join(alps_modules_disable)))
+
         return args
