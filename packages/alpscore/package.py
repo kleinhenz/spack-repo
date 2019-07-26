24cb8dac025fdbd9375fb74fd51a23b972a85c4f
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2019-07-26 17:40:56 -0400

    [alpscore] add hdf5 dependency

--- packages/alpscore/package.py
+++ packages/alpscore/package.py
@@ -9,6 +9,7 @@ class Alpscore(CMakePackage):
     version('master',       branch='master')
     version('2.2.0',        sha256='f7bc9c8f806fb0ad4d38cb6604a10d56ab159ca63aed6530c1f84ecaf40adc61')
 
+    depends_on('hdf5')
     depends_on('boost')
     depends_on('eigen ~fftw ~metis ~mpfr ~scotch ~suitesparse')
     variant('accumulators', default=False, description="Enable legacy accumulators")
